a7b6d8b936a390d71b28607d57736b5ac607d62d
hlebec <gleb.sidorenko007@gmail.com>
2026-09-25 19:21:57 +0300

    enable logger to write different levels depending on output

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -15,29 +15,50 @@ type Config struct {
 	OutputFile    string
 }
 
+type LevelWriter struct {
+	io.Writer
+	Level zerolog.Level
+}
+
+func (lw LevelWriter) WriteLevel(l zerolog.Level, p []byte) (n int, err error) {
+	if l >= lw.Level {
+		return lw.Writer.Write(p)
+	}
+	return len(p), nil
+}
+
 func Init(c Config) {
 	zerolog.TimeFieldFormat = time.RFC3339
 
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-
 	level, err := zerolog.ParseLevel(c.Level)
 	if err != nil {
-		zerolog.SetGlobalLevel(level)
+		level = zerolog.InfoLevel
 	}
+
+	//zerolog.SetGlobalLevel(level)
 
 	writers := make([]io.Writer, 0, 2)
 
 	if c.PrettyConsole {
-		writers = append(writers, &zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"})
+		writers = append(writers, LevelWriter{
+			&zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"},
+			level,
+		})
 	} else {
-		writers = append(writers, log.With().Caller().Logger())
+		writers = append(writers, LevelWriter{
+			zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger().Level(level),
+			level,
+		})
 	}
 
-	f, err := os.OpenFile(c.OutputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
-	if err != nil {
-		log.Warn().Err(err).Msg("Failed to open log file")
-	} else {
-		writers = append(writers, f)
+	if c.OutputFile != "" {
+		f, err := os.OpenFile(c.OutputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to open log file")
+		} else {
+			wr := zerolog.New(f).With().Timestamp().Logger()
+			writers = append(writers, LevelWriter{wr, level - 1})
+		}
 	}
 	log.Logger = log.Output(zerolog.MultiLevelWriter(writers...))
 
